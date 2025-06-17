1d354aa3c6e444d2c3c2b9d4e40c1f565c53ef97
Protheu <everaldo.sobrinho42@gmail.com>
2025-06-17 19:43:21 -0300

    feat: add cv model and default style for cv

---
+++ src/cvTemplate.jsx
@@ -0,0 +1,59 @@
+import "./cv.css";
+
+export function CvTemplate() {
+  return (
+    <>
+      <section>
+        <h1>Everaldo da Cruz Ferreira Leite Sobrinho</h1>
+        <p>Estrada da Boiúna, 2986, casa 3 22723-020, Rio de Janeiro, Rj</p>
+        <p>everaldo.sobrinho42@gmail.com</p>
+        <p>(021)98415-8569</p>
+      </section>
+      <h2>Objetivo:</h2>
+      <p>Me contrate!</p>
+      <h2>Formação:</h2>
+      <ul>
+        <li>
+          Segunda Grau Técnico em informática, Colégio Cenas, concluído em 2011
+        </li>
+        <li>
+          CS50x Introduction to Computer Science, Ciência da Computação, Havard
+          University, 2023
+        </li>
+      </ul>
+      <h2>Histórico profissional</h2>
+      <ul className="pratical-experience">
+        <li>
+          <h3>Reserva Mini</h3>
+          <p>Período: Novembro de 2024 - Janeiro de 2025</p>
+          <p>Cargo: Operador de Loja</p>
+          <p>Atividades:</p>
+          <ul>
+            <li>Atividade 1</li>
+            <li>Atividade 2</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Reserva Mini</h3>
+          <p>Período: Novembro de 2024 - Janeiro de 2025</p>
+          <p>Cargo: Operador de Loja</p>
+          <p>Atividades:</p>
+          <ul>
+            <li>Atividade 1</li>
+            <li>Atividade 2</li>
+          </ul>
+        </li>
+        <li>
+          <h3>Reserva Mini</h3>
+          <p>Período: Novembro de 2024 - Janeiro de 2025</p>
+          <p>Cargo: Operador de Loja</p>
+          <p>Atividades:</p>
+          <ul>
+            <li>Atividade 1</li>
+            <li>Atividade 2</li>
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+}
