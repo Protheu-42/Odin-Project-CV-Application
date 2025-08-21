224e25579d856acb09bd7e425c3bf0a9cf54ff63
Protheu <everaldo.sobrinho42@gmail.com>
2025-08-21 19:56:13 -0300

    Finish

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "../input.css";
+import "../cv.css";
 
 export function App() {
+  const [display, setDisplayMode] = useState("input");
   const [contact, setContact] = useState({
     name: "Temujin",
     address: "Mongolia",
@@ -9,31 +11,114 @@ export function App() {
     telephone: "555-pay-your-tribute-555",
   });
   const [goal, setGoal] = useState(
-    "Raze your lands, take your money, conquer your castle and leave on banner on the place"
+    "Raze your lands, take your money, conquer your castle and leave on my banner at the place"
   );
   const [academicFormations, setAcademicFormations] = useState({
     institution: "University of the Khan",
-    courseTitle: "Greater Khan formation",
-    ending: "doesn't matter",
+    courseTitle: "Greater Khan formation course",
+    ending: "Eternal Khan",
+  });
+  const [professional, setProfessional] = useState({
+    companyName: "Mongolian Empire",
+    timeAtCompany: "Forever and Before that",
+    position: "Emperor",
+    errants: "Conquer the World",
   });
 
-  function SubmitButton() {
-    return <p>loading...</p>;
+  if (display === "input") {
+    //Display Input
+    return (
+      <div className="input-display">
+        <Contact contact={contact} onChangeHandler={contactUpdater} />
+        <Objective goal={goal} onChangeHandler={objectiveUpdater} />
+        <AcademicExp
+          course={academicFormations}
+          onChangeHandler={academicFormationUpdater}
+        />
+        <ProfessionalExp
+          professional={professional}
+          onChangeHandler={professionalUpdater}
+        />
+        <button onClick={() => setDisplayMode("html")}>Submit</button>
+      </div>
+    );
+  } else {
+    //Display HTML
+    return (
+      <>
+        <div className="cv-display">
+          <section>
+            <h1>{contact.name}</h1>
+            <p>{contact.address}</p>
+            <p>{contact.email}</p>
+            <p>{contact.telephone}</p>
+          </section>
+          <h2>Objetivo:</h2>
+          <p>{goal}</p>
+          <h2>Formação</h2>
+          <ul>
+            <li>
+              {academicFormations.institution}, {academicFormations.courseTitle}
+              , {academicFormations.ending}
+            </li>
+          </ul>
+          <h2>Histórico profissional</h2>
+          <ul className="pratical-experience">
+            <li>
+              <h3>{professional.companyName}</h3>
+              <p>Período: {professional.timeAtCompany}</p>
+              <p>Cargo: {professional.position}</p>
+              <p>Atividades:</p>
+              <p>{professional.errants}</p>
+            </li>
+          </ul>
+
+          <button
+            onClick={() => {
+              setDisplayMode("input");
+            }}
+          >
+            Edit
+          </button>
+        </div>
+      </>
+    );
   }
 
-  return (
-    //Tudo abaixo tem que ser um if, e o else deve ser ativo pelo submit button
-    <div className="input-display">
-      <Contact contact={contact} onChangeHandler={contactUpdater} />
-      <Objective goal={goal} onChangeHandler={objectiveUpdater} />
-      <AcademicExp
-        course={academicFormations}
-        onChangeHandler={academicFormationUpdater}
-      />
-      <ProfissionalExp />
-      <button onClick={SubmitButton}>Submit</button>
-    </div>
-  );
+  function professionalUpdater(type, event) {
+    switch (type) {
+      case "companyName":
+        setProfessional((prevProfessional) => ({
+          ...prevProfessional,
+          companyName: event.target.value,
+        }));
+        break;
+
+      case "timeAtCompany":
+        setProfessional((prevProfessional) => ({
+          ...prevProfessional,
+          timeAtCompany: event.target.value,
+        }));
+        break;
+
+      case "position":
+        setProfessional((prevProfessional) => ({
+          ...prevProfessional,
+          position: event.target.value,
+        }));
+        break;
+
+      case "errants":
+        setProfessional((prevProfessional) => ({
+          ...prevProfessional,
+          errants: event.target.value,
+        }));
+        break;
+
+      default:
+        break;
+    }
+  }
 
   function contactUpdater(type, event) {
     switch (type) {
@@ -112,7 +197,6 @@ function Contact({ contact, onChangeHandler }) {
           value={contact.name}
           onChange={(event) => onChangeHandler("name", event)}
         />
-        <p>{contact.name}</p>
       </label>
 
       <label>
@@ -199,7 +283,7 @@ function AcademicExp({ course, onChangeHandler }) {
   );
 }
 
-function ProfissionalExp() {
+function ProfessionalExp({ professional, onChangeHandler }) {
   return (
     <section>
       <h1>Histórico Profissional</h1>
@@ -207,19 +291,36 @@ function ProfissionalExp() {
         <li className="input-section">
           <label>
             Nome da empresa
-            <input type="text" id="place" />
+            <input
+              type="text"
+              value={professional.companyName}
+              onChange={(event) => onChangeHandler("companyName", event)}
+            />
           </label>
           <label>
             Quando entrou e quando saiu da empresa
-            <input type="text" id="date" />
+            <input
+              type="text"
+              value={professional.timeAtCompany}
+              onChange={(event) => onChangeHandler("timeAtCompany", event)}
+            />
           </label>
           <label>
             Cargo
-            <input type="text" id="position" />
+            <input
+              type="text"
+              value={professional.position}
+              onChange={(event) => onChangeHandler("position", event)}
+            />
           </label>
           <label>
             Atividades
-            <textarea id="positionActivies" cols="30" rows="10"></textarea>
+            <textarea
+              value={professional.errants}
+              onChange={(event) => onChangeHandler("position", event)}
+              cols="30"
+              rows="10"
+            ></textarea>
           </label>
         </li>
       </ul>
